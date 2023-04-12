83ab40077817ca3714ddb7d4744d1830b8ba63c5
Matheus <matheusdesena18@gmail.com>
2023-04-11 21:52:48 -0300

    adicionando o script js

---
+++ portfolio-fiap/assets/js/scripts.js
@@ -0,0 +1,17 @@
+document.querySelector("#qtde").addEventListener("change", calcularOrcamento)
+document.querySelector("#js").addEventListener("change", calcularOrcamento)
+document.querySelector("#layout_sim").addEventListener("change", calcularOrcamento)
+document.querySelector("#layout_nao").addEventListener("change", calcularOrcamento)
+document.querySelector("#prazo").addEventListener("change", calcularOrcamento)
+
+function calcularOrcamento() {
+    
+    let preco = qtde.value * 100
+    if (js.checked) preco *= 1.1
+    if(layout_sim.checked) preco += 500
+
+    let taxaDeUrgencia = preco * (1.1 - prazo.value * 0.1)
+    preco += taxaDeUrgencia
+
+    output.innerHTML = "R$ " + preco.toFixed(2)
+}
