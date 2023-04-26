7c1752d83857de855afd7abe700f97b0874dbdec
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-25 22:48:49 -0300

    Commit

--- portfolio-fiap/assets/js/menu.js
+++ portfolio-fiap/assets/js/menu.js
@@ -1,6 +1,4 @@
-document.querySelector(".hamburguer")
-        .addEventListener("click", mostrar_menu)
-
-function mostrar_menu(){
-    document.querySelector(".menu").classList.toggle("show-menu")
-}
+document.querySelector("#hamburguer")
+        .addEventListener("click", 
+        ()=> document.querySelector("#menu").classList.toggle("show-menu")
+)
